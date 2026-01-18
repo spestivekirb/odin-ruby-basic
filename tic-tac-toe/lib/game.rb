07e8531a6fbbbd3a1d1840213dcb20a4f17f10d0
Jerry Wong <187459546+spestivekirb@users.noreply.github.com>
2026-01-18 18:13:24 -0500

    Finish writing game logic
    Game logic completed. Can place mark and check if game is finished.

--- tic-tac-toe/lib/game.rb
+++ tic-tac-toe/lib/game.rb
@@ -13,14 +13,32 @@ class Game
     @turn = 0
   end
 
-  def take_turn
-    @board.display_board
-    cur_player = player_move
-    puts "#{cur_player.name}, please enter the cell you would like to place your marker in (1-9)"
-    gets
+  def play_game
+    loop do
+      take_turn
+      result = @Board.declare_end
+      if result.is_a?(Player)
+        return "The winner is: #{result.name}!"
+      elsif result == 'Tie'
+        return "It's a draw!"
+      end
+    end
   end
 
   private
+
+  def take_turn
+    @board.display_board
+    cur_player = player_move
+    location = loop do
+      puts "#{cur_player.name}, please enter the cell you would like to place your marker in (1-9)"
+      response = gets.chomp
+      break response.to_i if @board.valid?(response.to_i)
+
+      puts 'Please enter a valid cell (1-9) that is not already occupied.'
+    end
+    @board.place_mark(cur_player, location)
+  end
 
   def player_move
     return @player1 if @turn.even?
